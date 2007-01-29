1065434b119a6eb285c9336c43de09ab3e8e8261
Jeffrey <jcollie@fedoraproject.org>
2007-01-29 09:24:33 -0600

    Move the default-setting code back outside of the loop.

--- hw-client/Profile.py
+++ hw-client/Profile.py
@@ -56,25 +56,26 @@ class Profile:
                 self.system = device['system']
             except:
                 pass
-# Defaults for when hardware doesnt return anything.  namely a new cpu type 
-            if self.platform == '':
-                self.platform = 'Unknown'
-            if self.bogomips == '':
-                self.bogomips = 1
-            if self.CPUVendor == '':
-                self.CPUVendor = 'Unknown'
-            if self.numCPUs == '':
-                self.numCPUs = 1
-            if self.CPUSpeed == '':
-                self.CPUSpeed = 0
-            if self.systemMemory == '':
-                self.systemMemory = 0
-            if self.systemSwap == '':
-                self.systemSwap = 0
-            if self.vendor == '':
-                self.vendor = 'Unknown'
-            if self.system == '':
-                self.system = 'Unknown'
+
+        # Defaults for when hardware doesnt return anything.  namely a new cpu type 
+        if self.platform == '':
+            self.platform = 'Unknown'
+        if self.bogomips == '':
+            self.bogomips = 1
+        if self.CPUVendor == '':
+            self.CPUVendor = 'Unknown'
+        if self.numCPUs == '':
+            self.numCPUs = 1
+        if self.CPUSpeed == '':
+            self.CPUSpeed = 0
+        if self.systemMemory == '':
+            self.systemMemory = 0
+        if self.systemSwap == '':
+            self.systemSwap = 0
+        if self.vendor == '':
+            self.vendor = 'Unknown'
+        if self.system == '':
+            self.system = 'Unknown'
 
 
     def get_host_string(self):
