3c79acd71d78dbb8ec49fd7ac844700270e2a774
mmcgrath@localhost.localdomain <>
2007-04-22 13:06:39 -0500

    Added desktop file and changed location of smolt icons

--- client/smoltGui.py
+++ client/smoltGui.py
@@ -192,9 +192,9 @@ class SmoltGui(object):
             self.aboutDialog.set_wrap_license(True)
             self.aboutDialog.set_license('This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.')
             if os.path.exists('smolt-about.png'):
-                logo = gtk.gdk.pixbuf_new_from_file('smolt-about.png')
+                logo = gtk.gdk.pixbuf_new_from_file('icons/smolt-about.png')
             else:
-                logo = gtk.gdk.pixbuf_new_from_file('/usr/share/smolt/client/smolt-about.png')
+                logo = gtk.gdk.pixbuf_new_from_file('/usr/share/smolt/client/icons/smolt-about.png')
             self.aboutDialog.set_logo(logo)
             self.aboutDialog.connect('response', self.about_response_cb)
             self.aboutDialog.connect('close', self.about_close_cb)
