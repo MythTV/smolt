88bc133072896fbd056ec9e3f78645d546808b95
Yaakov Nemoy <loupgaroublond@gmail.com>
2007-06-26 16:55:49 -0400

    Working on caching, commiting so I can work at home

---
+++ smoon/hardware/threading/multilock.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python
+from __future__ import with_statement
+
+from threading import Lock, Event
+
+
+class ReverseSemaphore(object):
+    def __init__(self):
+        self.counter = 0
+        self.lock = Lock()
+        self.event = Event()
+        self.event.set()
+        pass
+        
+    def acquire(self):
+        with self.lock:
+            self.counter += 1
+            self.event.clear()
+            pass
+        pass
+    
+    def release(self):
+        with self.lock:
+            self.counter -= 1
+            if self.counter == 0:
+                self.event.set()
+            if self.counter < 0:
+                self.counter = 0
+                pass
+            pass
+        pass
+    
+    def wait(self):
+        return self.event.wait()
+    pass
+
+class MultiLock(object):
+    def __init__(self):
+        self.write_lock = Lock()
+        self.read_lock = ReverseSemaphore()
+        self.write_event = Event()
+        self.write_event.set()
+        pass
+    
+    def read_transaction(self):
+        pass
+    
+    def read_acquire(self):
+        self.read_lock.acquire()
+        pass
+    
+    def read_release(self):
+        self.read_lock.release()
+    
+    def write_acquire(self):
+        self.write_lock.acquire() 
+        self.write_event.clear()
+        self.read_lock.wait()
+        pass
+    
+    def write_release(self):
+        self.write_event.set()
+        self.write_lock.release()
+    pass
