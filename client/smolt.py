3d9ee1e224131a668007acbdf25f2a2ccbcab788
Yaakov Nemoy <loupgaroublond@gmail.com>
2007-07-14 00:46:09 -0400

    branch:      main added a default for default on smolt.get_config_attr()

--- client/smolt.py
+++ client/smolt.py
@@ -43,7 +43,7 @@ from urllib import urlencode
 
 import config
 
-def get_config_attr(attr, default):
+def get_config_attr(attr, default=""):
     if hasattr(config, attr):
         return getattr(config, attr)
     else:
