298e06f2a8ded7f1f983a86555ae3f8ac7699272
Yaakov Nemoy <loupgaroublond@gmail.com>
2007-08-06 12:21:51 -0400

    branch:      main Added support for Frugalware, Mythvantage, Slackware, Archlinux, Crux, and Chuck Norris. (James Meyer)

--- client/config.py
+++ client/config.py
@@ -1,4 +1,8 @@
+
+import os
+
 SMOON_URL = "http://smolt.fedoraproject.org/"
+
 
 #Only a fool would edit what lays beyond here
 #Are you that fool?
@@ -18,7 +22,6 @@ SMOON_URL = "http://smolt.fedoraproject.org/"
 #except IOError:
 #    OS = "It's a Lizard man!, It changes Colours!"
 #
-#HW_UUID = "/etc/smolt/hw-uuid
 #
 #For Debian
 try:
@@ -30,4 +33,50 @@ try:
 except IOError:
     OS = "The swirl, it's Spinning!"
 
+
+##For Frugalware
+#try:
+#    OS = file('/etc/frugalware-release').read().strip()
+#except IOError:
+#    OS = "Too cheap to have a version!"
+#
+##For Mythvantage
+#try:
+#    OS = file('/etc/mythvantage-release').read().strip()
+#except IOError:
+#    OS = "The OS doesn't really exist but in the minds of Holywood"
+#
+##For Slackware
+#try:
+#    OS = file('/etc/slackware-version').read().strip()
+#except IOError:
+#    OS = "Quit slacking you kids!"
+#
+##For Archlinux
+#try:
+#    if os.path.exists('/etc/arch-release'):
+#        archfile = file('/etc/issue')
+#        archfile.readline()
+#        archrelease = archfile.readline()
+#        archrelease = archrelease.split('\\r')
+#        OS = archrelease[0]
+#    else:
+#        OS = "It's an impossibly tall Arch!"
+#except IOError:
+#    OS = "It's an impossibly tall Arch!"
+#    
+##For Crux
+#try:
+#    if os.path.exists('/etc/issue'):
+#        cruxfile = open('/etc/issue')
+#        cruxrelease = cruxfile.read().strip()
+#        if cruxrelease.find("CRUX") >= 0:
+#            return "CRUX"
+#    else:
+#        return "Chuck Norris"
+#except IOError:
+#    return "Chuck Norris"
+
+
+#For non RH Distros
 HW_UUID = "/etc/smolt/hw-uuid"
