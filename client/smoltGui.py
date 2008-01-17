1b0dead069610617e5dec63ae7f6309602e64e94
Jeffrey C. Ollie <jeff@ocjtech.us>
2008-01-17 00:07:08 -0600

    Fix path to about icon when running from source checkout.

--- client/smoltGui.py
+++ client/smoltGui.py
@@ -213,7 +213,7 @@ class SmoltGui(object):
             self.aboutDialog.set_copyright(_('Copyright © 2007 Mike McGrath'))
             self.aboutDialog.set_wrap_license(True)
             self.aboutDialog.set_license(_('This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.'))
-            if os.path.exists('smolt-about.png'):
+            if os.path.exists('icons/smolt-about.png'):
                 logo = gtk.gdk.pixbuf_new_from_file('icons/smolt-about.png')
             else:
                 logo = gtk.gdk.pixbuf_new_from_file('/usr/share/smolt/client/icons/smolt-about.png')
