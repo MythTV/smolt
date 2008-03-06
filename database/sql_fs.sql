a35e63f72ee0233fcda2b74ffa4de7cd3043d518
Michael McGrath <mmcgrath@desktop.mmcgrath.net>
2008-03-06 16:22:31 -0600

    no need for only smoon to access

--- database/sql_fs.sql
+++ database/sql_fs.sql
@@ -16,6 +16,5 @@ DROP TABLE IF EXISTS `FILESYSTEMS`;
 /*!50001 DROP TABLE IF EXISTS `FILESYSTEMS`*/;
 /*!50001 DROP VIEW IF EXISTS `FILESYSTEMS`*/;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`smoon`@`localhost` SQL SECURITY DEFINER */
 /*!50001 VIEW `FILESYSTEMS` AS select `file_systems`.`fs_type` AS `fs_type`,count(`file_systems`.`fs_type`) AS `cnt` from `file_systems` group by `file_systems`.`fs_type` order by count(`file_systems`.`fs_type`) desc */;
 
