7a66d79f8d6e002d79c5d45540f22c8cb3ad6c80
Sebastian Pipping <sebastian@pipping.org>
2009-07-16 21:36:31 +0200

    No longer collect arguably useful USE_ORDER

--- client/distros/gentoo/trivialscalars.py
+++ client/distros/gentoo/trivialscalars.py
@@ -21,7 +21,7 @@ import portage
 class TrivialScalars:
     def __init__(self):
         self._trivial_scalars = {}
-        for k in ('ARCH', 'CHOST', 'USE_ORDER'):
+        for k in ('ARCH', 'CHOST'):
             self._trivial_scalars[k] = portage.settings[k].strip()
 
     def get(self):
