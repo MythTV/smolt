51235766caabf746738b15000bddd9f885f531a5
Sebastian Pipping <sebastian@pipping.org>
2009-07-17 03:19:50 +0200

    Move to new main() location in a backwards-compatible way

--- client/distros/gentoo.py
+++ client/distros/gentoo.py
@@ -16,58 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 
-import sys
-import os
-from gentoo.compileflags import CompileFlags
-from gentoo.globaluseflags import GlobalUseFlags
-from gentoo.installedpackages import InstalledPackages
-from gentoo.makeopts import MakeOpts
-from gentoo.mirrors import Mirrors
-from gentoo.overlays import Overlays
-from gentoo.packagestar import PackageMask
-from gentoo.systemprofile import SystemProfile
-from gentoo.trivialscalars import TrivialScalars
-from gentoo.trivialvectors import TrivialVectors
-
-def stage(text):
-    print 'Processing %s' % (text)
+from gentoo.main import main
 
 if __name__ == '__main__':
-    # Enable auto-flushing for stdout
-    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-
-    stage('global use flags')
-    global_use_flags = GlobalUseFlags()
-
-    stage('compile flags')
-    compile_flags = CompileFlags()
-
-    stage('make opts')
-    make_opts = MakeOpts()
-
-    stage('mirrors')
-    mirrors = Mirrors()
-
-    stage('overlays')
-    overlays = Overlays()
-
-    stage('package.mask entries')
-    user_package_mask = PackageMask()
-
-    stage('system profile')
-    system_profile = SystemProfile()
-
-    stage('trivial scalars')
-    trivial_scalars = TrivialScalars()
-
-    stage('trivial vectors')
-    trivial_vectors = TrivialVectors()
-
-    stage('installed packages (takes some time)')
-    installed_packages = InstalledPackages(debug=True)
-
-    print
-    for i in (installed_packages, global_use_flags, compile_flags,
-            make_opts, mirrors, overlays, user_package_mask, system_profile,
-            trivial_scalars, trivial_vectors):
-        i.dump()
+    main()
