79bccb1d2cd701b8ef9c1a5890740d3a6374605b
Sebastian Pipping <sebastian@pipping.org>
2009-07-28 20:03:38 +0200

    Move to Python code generation and exec()

--- smoon/playmodel.py
+++ smoon/playmodel.py
@@ -22,147 +22,155 @@ from sqlalchemy.orm import mapper, relation
 metadata = MetaData()
 
 
-class ClassNameCollision(Exception):
-    pass
+_pool_table_jobs = [
+    {'thing':'atom', 'col_type':'CHAR(255)'},
+    {'thing':'cflag', 'col_type':'CHAR(255)'},
+    {'thing':'chost', 'col_type':'CHAR(255)'},
+    {'thing':'cxxflag', 'col_type':'CHAR(255)'},
+    {'thing':'feature', 'col_type':'CHAR(127)'},
+    {'thing':'ldflag', 'col_type':'CHAR(255)'},
+    {'thing':'makeopt', 'col_type':'CHAR(255)'},
+    {'thing':'keyword', 'col_type':'CHAR(127)'},
+    {'thing':'mirror', 'col_type':'CHAR(255)'},
+    {'thing':'package', 'col_type':'CHAR(255)'},
+    {'thing':'repository', 'col_type':'CHAR(127)'},
+    {'thing':'system_profile', 'col_type':'CHAR(255)'},
+    {'thing':'use_flag', 'col_type':'CHAR(127)'},
+    {'thing':'version', 'col_type':'CHAR(127)'},
+]
+
+_rel_table_jobs = [
+    {'thing':'accept_keywords', 'foreign':'keyword', 'vector':True},
+    {'thing':'archs', 'foreign':'keyword', 'vector':False},
+    {'thing':'chosts', 'foreign':'chost', 'vector':False},
+    {'thing':'sync_mirrors', 'foreign':'mirror', 'vector':False},
+    {'thing':'cflags', 'foreign':'cflag', 'vector':True},
+    {'thing':'cxxflags', 'foreign':'cxxflag', 'vector':True},
+    {'thing':'distfiles_mirrors', 'foreign':'mirror', 'vector':True},
+    {'thing':'features', 'foreign':'feature', 'vector':True},
+    {'thing':'global_use_flags', 'foreign':'use_flag', 'vector':True},
+    {'thing':'ldflags', 'foreign':'ldflag', 'vector':True},
+    {'thing':'makeopts', 'foreign':'makeopt', 'vector':True},
+    {'thing':'overlays', 'foreign':'repository', 'vector':True},
+    {'thing':'system_profile', 'foreign':'system_profile', 'vector':False},
+]
 
 
-def _register_class(class_name, class_object):
-    if class_name in globals():
-        raise ClassNameCollision
-    class_object.__name__ = class_name
-    globals()[class_name] = class_object
-
-
-_pool_tables = {
-    'keyword':{'pool_type':CHAR(127)},
-    'chost':{'pool_type':CHAR(255)},
-    'compile_flag':{'pool_type':CHAR(255)},
-    'mirror':{'pool_type':CHAR(255)},
-    'overlay':{'pool_type':CHAR(127)},
-    'feature':{'pool_type':CHAR(127)},
-    'package':{'pool_type':CHAR(127)},
-#   'use_flag':{'pool_type':CHAR(127)},
-}
-
-_map_tables = {
-    'archs':{'foreign_name':'keyword', 'vector':False},
-    'chosts':{'foreign_name':'chost', 'vector':False},
-    'sync_mirrors':{'foreign_name':'mirror', 'vector':False},
-    'keywords':{'foreign_name':'keyword', 'vector':True},
-    'cflags':{'foreign_name':'compile_flag', 'vector':True},
-    'cxxflags':{'foreign_name':'compile_flag', 'vector':True},
-    'ldflags':{'foreign_name':'compile_flag', 'vector':True},
-    'makeopts':{'foreign_name':'compile_flag', 'vector':True},
-    'features':{'foreign_name':'feature', 'vector':True},
-    'distfiles_mirrors':{'foreign_name':'mirror', 'vector':True},
-#   'global_use_flags':{'foreign_name':'use_flag', 'vector':True},
-}
-
-_generated_tables = {}
-
-for k, v in _pool_tables.items():
-    table_name = 'gentoo_%s_pool' % k
-    class_name = 'Gentoo%sString' % k.title().replace('_', '')
-    pool_type = v['pool_type']
-    logging.debug('Generating table "%s" and related class "%s"...' % (table_name, class_name))
-
-    # Create table
-    _generated_tables[table_name] = Table(table_name, metadata,
-        Column('id', Integer, primary_key=True, autoincrement=True),
-        Column('name', pool_type, unique=True),
-    )
-
-    # Create class
-    class generated_class_object(object):
-        def __init__(self, name):
-            self.name = name
-    _register_class(class_name, generated_class_object)
-
-    # Setup mapping
-    mapper(generated_class_object, _generated_tables[table_name])
-
-    # Store names for use by other tables
-    v['table_name'] = table_name
-    v['class_name'] = class_name
-
-
-for k, v in _map_tables.items():
-    table_name = 'gentoo_%s' % k
-    class_name = 'Gentoo%s' % k.rstrip('s').title().replace('_', '')
-    foreign_column_name = '%s_id' % v['foreign_name']
-    foreign_relation_name = v['foreign_name']
-    foreign_key_name = '%s.id' % _pool_tables[v['foreign_name']]['table_name']
-    foreign_key_class_name = _pool_tables[v['foreign_name']]['class_name']
-    foreign_key_class = globals()[foreign_key_class_name]
-    vector = v['vector']
-    logging.info('Generating table "%s" and related class "%s"...' % (table_name, class_name))
-
-    # Create table
-    if vector:
-        _generated_tables[table_name] = Table(table_name, metadata,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('machine_id', Integer),
-            Column(foreign_column_name, Integer, ForeignKey(foreign_key_name)),
-            UniqueConstraint('machine_id', foreign_column_name),
-        )
-    else:
-        _generated_tables[table_name] = Table(table_name, metadata,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('machine_id', Integer, unique=True),
-            Column(foreign_column_name, Integer, ForeignKey(foreign_key_name)),
-        )
-
-    # Create class
-    class generated_class_object(object):
-        def __init__(self, machine_id, foreign_id):
-            self.machine_id = machine_id
-            setattr(self, foreign_column_name, foreign_id)
-    _register_class(class_name, generated_class_object)
-
-    # Setup mapping
-    mapper(generated_class_object, _generated_tables[table_name],
-        properties={
-            foreign_relation_name:relation(foreign_key_class),
-        }
-    )
-
-# TODO
-#     packages
-#     package use flags
-
-
-# =========================================================
-# gentoo_use_flag_pool
-# =========================================================
-_gentoo_use_flag_pool_table = Table('gentoo_use_flag_pool', metadata,
+_POOL_TABLE_TEMPLATE = """
+%(table_var_name)s = Table('%(table_name)s', metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('name', CHAR(255), unique=True),
+    Column('name', %(col_type)s, unique=True),
 )
 
-class GentooUseFlagString(object):
+class %(class_name)s(object):
     def __init__(self, name):
         self.name = name
 
-mapper(GentooUseFlagString, _gentoo_use_flag_pool_table)
+mapper(%(class_name)s, %(table_var_name)s)
+"""
 
 
-# =========================================================
-# gentoo_global_use_flags
-# =========================================================
-_gentoo_global_use_flags_table = Table('gentoo_global_use_flags', metadata,
+_SCALAR_REL_TABLE_TEMPLATE = """
+%(table_var_name)s = Table('%(table_name)s', metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('machine_id', Integer),
-    Column('use_flag_id', Integer, ForeignKey('gentoo_use_flag_pool.id')),
-    UniqueConstraint('machine_id', 'use_flag_id'),
+    Column('machine_id', Integer, primary_key=True),
+    Column('%(foreign_key_column)s', Integer, ForeignKey('%(foreign_key_table)s.id')),
 )
 
-class GentooGlobalUseFlag(object):
-    def __init__(self, machine_id, use_flag_id):
+class %(class_name)s(object):
+    def __init__(self, machine_id, %(foreign_key_column)s):
         self.machine_id = machine_id
-        self.use_flag_id = use_flag_id
+        self.%(foreign_key_column)s = %(foreign_key_column)s
 
-mapper(GentooGlobalUseFlag, _gentoo_global_use_flags_table,
+mapper(%(class_name)s, %(table_var_name)s,
     properties={
-        'use_flag':relation(GentooUseFlagString),
+        '%(relation_name)s':relation(%(foreign_key_class)s),
     }
 )
+"""
+
+
+_VECTOR_REL_TABLE_TEMPLATE = """
+%(table_var_name)s = Table('%(table_name)s', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('machine_id', Integer),
+    Column('%(foreign_key_column)s', Integer, ForeignKey('%(foreign_key_table)s.id')),
+    UniqueConstraint('machine_id', '%(foreign_key_column)s'),
+)
+
+class %(class_name)s(object):
+    def __init__(self, machine_id, %(foreign_key_column)s):
+        self.machine_id = machine_id
+        self.%(foreign_key_column)s = %(foreign_key_column)s
+
+mapper(%(class_name)s, %(table_var_name)s,
+    properties={
+        '%(relation_name)s':relation(%(foreign_key_class)s),
+    }
+)
+"""
+
+
+def _pool_table_name(middle):
+    return 'gentoo_%s_pool' % middle.rstrip('s')
+
+def _pool_table_instance(middle):
+    return '_gentoo_%s_pool_table' % middle.rstrip('s')
+
+def _pool_class_name(middle):
+    return 'Gentoo%sString' % middle.title().replace('_', '')
+
+def _rel_table_name(middle):
+    return 'gentoo_%s_rel' % middle.rstrip('s')
+
+def _rel_table_instance(middle):
+    return '_gentoo_%s_rel_table' % middle.rstrip('s')
+
+def _rel_class_name(middle):
+    return 'Gentoo%s' % middle.rstrip('s').title().replace('_', '')
+
+def _foreign_key_column(middle):
+    return '%s_id' % middle.rstrip('s')
+
+
+# Create pool tables
+for job in _pool_table_jobs:
+    thing = job['thing']
+    col_type = job['col_type']
+    details = {
+        'table_name':_pool_table_name(thing),
+        'table_var_name':_pool_table_instance(thing),
+        'class_name':_pool_class_name(thing),
+        'col_type':col_type,
+    }
+    logging.debug('Generating table "%(table_name)s" and related class "%(class_name)s"...' % details)
+    program = _POOL_TABLE_TEMPLATE % details
+    print "========================="
+    print program
+    print "========================="
+    exec(program)
+
+
+# Create relation tables
+for job in _rel_table_jobs:
+    thing = job['thing']
+    foreign = job['foreign']
+    vector = job['vector']
+    details = {
+        'table_name':_rel_table_name(thing),
+        'table_var_name':_rel_table_instance(thing),
+        'class_name':_rel_class_name(thing),
+        'foreign_key_table':_pool_table_name(foreign),
+        'foreign_key_column':_foreign_key_column(foreign),
+        'foreign_key_class':_pool_class_name(foreign),
+        'relation_name':foreign,
+    }
+    logging.debug('Generating table "%(table_name)s" and related class "%(class_name)s"...' % details)
+    if vector:
+        program = _VECTOR_REL_TABLE_TEMPLATE % details
+    else:
+        program = _SCALAR_REL_TABLE_TEMPLATE % details
+    print "========================="
+    print program
+    print "========================="
+    exec(program)
