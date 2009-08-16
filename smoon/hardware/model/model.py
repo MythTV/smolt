ccc1cc4266a41f2338c5cc4a94f67f44f65c9307
Sebastian Pipping <sebastian@pipping.org>
2009-08-16 02:57:35 +0200

    Server: Fix data column data type

--- smoon/hardware/model/model.py
+++ smoon/hardware/model/model.py
@@ -133,7 +133,7 @@ batch_queue = Table('batch_queue', metadata,
                             nullable=False),
                     Column('hw_uuid', VARCHAR(36),
                             nullable=False),
-                    Column('data', Text(1000000)))
+                    Column('data', CLOB(1000000)))
 
 
 class Host(object):
