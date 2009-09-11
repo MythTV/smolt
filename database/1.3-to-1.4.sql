cd3c45ed3e632b1d1c2c8c06b4d6ea63e507c6d4
Mike McGrath <mmcgrath@redhat.com>
2009-09-11 17:20:25 -0500

    This is possible for 1.4
    I don't want to forget it

--- database/1.3-to-1.4.sql
+++ database/1.3-to-1.4.sql
@@ -4,5 +4,7 @@ create index fs_join_index on file_systems (host_id, fs_type);
 create index last_modified_join on host (id, last_modified);
 -- 
 create index device_host_link on host_links (device_id, host_link_id);
+-- This one is a maybe, don't want to forget it
+-- create index system_vendor on host (system, vendor);
 -- This is a duplicate, dropping 5 seconds
 drop index class_2 on device;
