14600fec0a1b9456c9a126eab249f26444192d1c
Mike McGrath <mmcgrath@redhat.com>
2010-01-26 13:14:42 -0600

    Changing to using ugettext

--- client/i18n.py
+++ client/i18n.py
@@ -39,4 +39,4 @@ except IndexError:
     locale.setlocale(locale.LC_ALL, 'C')
     t = gettext.translation('smolt', '/usr/share/locale/', fallback=True, languages='en_US')
 
-_ = t.gettext
+_ = t.ugettext
