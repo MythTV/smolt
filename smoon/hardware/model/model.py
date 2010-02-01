7e7cd7395f3e2c5b1245c71c7bc2909178f00537
Mike McGrath <mmcgrath@redhat.com>
2010-02-01 13:14:37 -0600

    Adding host archives table

--- smoon/hardware/model/model.py
+++ smoon/hardware/model/model.py
@@ -40,6 +40,18 @@ host_links = Table('host_links', metadata,
                           ForeignKey("device.id")),
                    Column("rating", INT))
 
+host_links_archive = Table('host_links_archive', metadata,
+                   Column("id", INT,
+                          autoincrement=True,
+                          nullable=False,
+                          primary_key=True),
+                   Column('host_link_id', INT,
+                          ForeignKey("host.id"),
+                          nullable=False),
+                   Column("device_id", INT,
+                          ForeignKey("device.id")),
+                   Column("rating", INT))
+
 hosts = Table('host', metadata,
               Column("id", INT,
                      autoincrement=True,
@@ -78,6 +90,42 @@ hosts = Table('host', metadata,
 #              Column('myth_systemrole', TEXT),
 #              Column('mythremote', TEXT),
 #              Column('myththeme', TEXT))
+
+hosts_archive = Table('host_archive', metadata,
+              Column("id", INT,
+                     autoincrement=True,
+                     nullable=False,
+                     primary_key=True),
+              Column('uuid', VARCHAR(36),
+                     nullable=False,
+                     unique=True),
+              Column('pub_uuid', VARCHAR(40),
+                     nullable=False,
+                     unique=True),
+              Column('os', TEXT),
+              Column('platform', TEXT),
+              Column('bogomips', DECIMAL),
+              Column('system_memory', INT),
+              Column('system_swap', INT),
+              Column('vendor', TEXT),
+              Column('system', TEXT),
+              Column('cpu_vendor', TEXT),
+              Column('cpu_model', TEXT),
+              Column('num_cpus', INT),
+              Column('cpu_speed', DECIMAL),
+              Column('language', TEXT),
+              Column('default_runlevel', INT),
+              Column('kernel_version', TEXT),
+              Column('formfactor', TEXT),
+              Column('last_modified', DATETIME,
+                     default=0, nullable=False),
+              Column('rating', INT, nullable=False, default=0),
+              Column('selinux_enabled', INT, nullable=False),
+              Column('selinux_policy', TEXT),
+              Column('selinux_enforce', TEXT),
+              Column('cpu_stepping', INT, default=None),
+              Column('cpu_family', INT, default=None),
+              Column('cpu_model_num', INT, default=None))
 
 fas_links = Table('fas_link', metadata,
                   Column("id", INT, autoincrement=True,
@@ -120,10 +168,18 @@ class Host(object):
         self.rating = rating
         self.last_modified = last_modified
 
+class HostArchive(object):
+    def __init__(self, rating=0):
+        self.rating = rating
+
 class ComputerLogicalDevice(object):
     pass
 
 class HostLink(object):
+    def __init__(self, rating=0):
+        self.rating = rating
+
+class HostLinkArchive(object):
     def __init__(self, rating=0):
         self.rating = rating
 
@@ -155,6 +211,8 @@ mapper(Host, hosts,
                       file_systems=relation(FileSystem,
                                             backref='host')))
 
+mapper(HostArchive, hosts_archive)
+
 mapper(ComputerLogicalDevice,
        computer_logical_devices,
        properties = {"_host_links": relation(HostLink,
@@ -165,6 +223,7 @@ mapper(ComputerLogicalDevice,
                                             cascade="all,delete-orphan")})
 
 mapper(HostLink, host_links)
+mapper(HostLinkArchive, host_links_archive)
 
 mapper(FasLink, fas_links, properties = {'hosts': relation(Host),
                                          'uuid': fas_links.c.uuid})
